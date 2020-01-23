206c7720982071d567e1586bf5cd12c099149112
Hugo Marcelo e Silva <hugo.marcelo@lg.com.br>
2020-01-22 22:04:05 -0300

    feat: configured dotenv

--- packages/backend/src/index.js
+++ packages/backend/src/index.js
@@ -1,3 +1,5 @@
+require('dotenv/config');
+
 const cors = require('cors');
 const express = require('express');
 const http = require('http');
@@ -14,10 +16,11 @@ setupWebSocket(server);
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-server.listen(3333);
+server.listen(process.env.PORT || 3333);
