54ed81cf3b24d34d10a9bd349021c1631902e99e
Hugo Marcelo e Silva <hugomarcelo91@gmail.com>
2020-01-24 18:29:57 -0300

    feat: create .gitignore on backend

--- packages/backend/src/index.js
+++ packages/backend/src/index.js
@@ -19,11 +19,6 @@ mongoose.connect(process.env.MONGO_URL, {
   useCreateIndex: true,
 });
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   next();
-// });
-
 app.use(cors());
 app.use(express.json());
 app.use(routes);
